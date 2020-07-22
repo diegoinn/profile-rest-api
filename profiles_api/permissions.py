21cbc2967c777d7d5894d49059bf278018126e40
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-07-22 00:46:57 -0500

    Added login API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,7 @@ class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
-        """Check user is trying to edit their  own profile"""
+        """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
 
